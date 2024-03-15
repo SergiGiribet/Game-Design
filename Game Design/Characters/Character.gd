49e076a4e532282f88ca37642a14d22cc3e74b6e
unknown <sergigiribet2005@ginebro.cat>
2024-03-15 22:45:05 +0100

    Character.gd

--- Game Design/Characters/Character.gd
+++ Game Design/Characters/Character.gd
@@ -1,37 +1,22 @@
 @icon("res://assets/kenney_scribbledungeons/Player.png")
 
 extends CharacterBody2D
-
 class_name Character
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const FRICTION: float = 0.15
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@export var accerelation: int = 40
+@export var max_speed: int = 100
 
+var mov_direction: Vector2 = Vector2.ZERO
 
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	## Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction_x = Input.get_axis("ui_left(a)", "ui_right(d)")
-	if direction_x:
-		velocity.x = direction_x * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
-	var direction_y  = Input.get_axis("ui_up(w)", "ui_down(s)")
-	if direction_y:
-		velocity.y = direction_y * SPEED
-	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
-
+func _physics_process(_delta: float) -> void:
 	move_and_slide()
+	velocity = lerp(velocity, Vector2.ZERO, FRICTION)
+
+
+func move() -> void:
+	mov_direction = mov_direction.normalized()
+	velocity += mov_direction * accerelation
+	velocity = velocity.limit_length(max_speed)
+
