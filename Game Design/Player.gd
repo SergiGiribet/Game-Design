b4ac99d156f7fc3d60cd530a5d9ed0c3b7ae1bf4
arnau clavijos roger <ArnauClavijos2004@ginebro.cat>
2024-03-16 01:14:17 +0100

    first attack animation

--- Game Design/Player.gd
+++ Game Design/Player.gd
@@ -3,12 +3,15 @@
 extends Character
 
 @onready var weapon: Node2D = get_node("Weapon")
+@onready var weapon_animation_player: AnimationPlayer = weapon.get_node("WeaponAnimationPlayer")
 
 @onready var sprite = $Sprite2D
 
 func _process(_delta: float) -> void:
 	var mouse_position: Vector2 = get_global_mouse_position()
 	self.look_at(mouse_position)
+	if Input.is_action_pressed("ui_attack") and not weapon_animation_player.is_playing():
+		weapon_animation_player.play("attack")
 
 func get_input() -> void:
 	mov_direction = Vector2.ZERO
